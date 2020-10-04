fb2e820400eeded9cd3a21627dae377c73c821c9
Melikbekyan Ashot <41271647+MelikbekyanAshot@users.noreply.github.com>
2020-10-04 20:41:05 +0300

    Add files via upload

--- AMlab5/src/Test.java
+++ AMlab5/src/Test.java
@@ -4,12 +4,12 @@ public class Test {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
         System.out.println("Test 9 task. Enter amount of zeroes and units:");
-        Task9Solution.without2Zero(keyboard.nextInt() ,keyboard.nextInt());
+        System.out.println(Task9Solution.without2Zero(keyboard.nextInt(), keyboard.nextInt()));
         System.out.println("Test 10 task. Enter number:");
-        Task10Solution.reverseNumber(keyboard.nextInt());
-        //Task11Solution
+        System.out.println(Task10Solution.reverseNumber(keyboard.nextInt()));
         System.out.println("Test 12 task. ");
         Task12Solution.isOdd();
+        System.out.println("Test 13 task. ");
         Task13Solution.printOddIndex();
     }
 }
